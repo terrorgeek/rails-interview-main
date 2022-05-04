86556610be3533ba4055a91d168a64cab90e0706
painforever <bjslxxx@126.com>
2022-05-04 13:47:21 -0400

    a

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 RSpec.describe SessionsController do
   let!(:user_plumber) { create(:user, email: "the@plumber.com", password: "12345678") }
+  let(:login_action) {
+    post "create", params: {login: { email: "the@plumber.com", password: "wrong_password" }} 
+  }
 
   describe "Sessions #create" do
     it "Should render :new after failed login" do
-      post "create", params: {login: { email: "the@plumber.com", password: "wrong_password" }} 
+      #post "create", params: {login: { email: "the@plumber.com", password: "wrong_password" }} 
+      login_action
       expect(response).to render_template(:new)
       expect(assigns(:my_instance_variable)).to eq("Yu Song")
     end
@@ -15,9 +19,10 @@ RSpec.describe SessionsController do
     #   }.to change { ActionMailer::Base.deliveries.count }.by(1)
     # end
 
-    it 'When login the email should be sent by Sidekiq' do 
-      expect{ 
-        post "create", params: {login: { email: "the@plumber.com", password: "wrong_password" }} 
+    it 'When login the email should be enqueued' do 
+      expect { 
+        #post "create", params: {login: { email: "the@plumber.com", password: "wrong_password" }} 
+        login_action
       }.to have_enqueued_mail(UserMailer, :send_email)
     end
   end
